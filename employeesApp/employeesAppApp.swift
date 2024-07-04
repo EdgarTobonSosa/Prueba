408c87e7c5a06192936c7b52e2eb845f89542987
Edgar Tobon Sosa <edgar.tobon@elektra.com.mx>
2024-07-04 14:05:52 -0600

    Initial Commit

---
+++ employeesApp/employeesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  employeesAppApp.swift
+//  employeesApp
+//
+//  Created by Edgar Tobón Sosa on 04/07/24.
+//
+
+import SwiftUI
+
+@main
+struct employeesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
