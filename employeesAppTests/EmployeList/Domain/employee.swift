8dc0ddbd43ba4ea937a0cc603a323a77194f9802
Edgar Tobon Sosa <edgar.tobon@elektra.com.mx>
2024-07-04 14:25:01 -0600

    [Domain] Entitie Employee + Response finish

---
+++ employeesAppTests/EmployeList/Domain/employee.swift
@@ -0,0 +1,52 @@
+//
+//  employee.swift
+//  employeesAppTests
+//
+//  Created by Edgar Tobón Sosa on 04/07/24.
+//
+
+import Foundation
+
+struct EmployeeResponse: Codable {
+    let status: String
+    let data: [Employee]
+    let message: String
+    
+    enum CodignKeys: String, CodingKey {
+        case status, data, message
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodignKeys.self)
+        
+        self.status = try container.decode(String.self, forKey: .status)
+        self.data = try container.decode([Employee].self, forKey: .data)
+        self.message = try container.decode(String.self, forKey: .message)
+    }
+}
+
+struct Employee: Codable {
+    let id: Int
+    let name: String
+    let salary: Int
+    let age: Int
+    let image: String
+    
+    enum CodignKeys: String,CodingKey {
+        case id = "id"
+        case name = "employee_name"
+        case salary = "employee_salary"
+        case age = "employee_age"
+        case image = "profile_image"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodignKeys.self)
+        
+        self.id  = try container.decode(Int.self, forKey: .id)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.salary = try container.decode(Int.self, forKey: .salary)
+        self.age = try container.decode(Int.self, forKey: .age)
+        self.image = try container.decode(String.self, forKey: .image)
+    }
+}
